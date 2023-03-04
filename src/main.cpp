5dc9a29c7dc0bb6bd774d3e2a3e1962e1b057043
Steffanus W <swiedjaja@gmail.com>
2023-03-04 16:45:50 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#define LED_GREEN   4
+#define LED_YELLOW  5
+#define LED_RED    18
+
+QueueHandle_t xQueue;
+
+void taskProduce(void *pvParameters) {
+  int i = 0;
+  while (1) {
+    Serial.printf("==> Produce: %d \n", i); fflush(stdout);
+    xQueueSend(xQueue, &i, portMAX_DELAY);
+    i++;
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void taskConsume(void *pvParameters) {
+  int i;
+  while (1) {
+    xQueueReceive(xQueue, &i, portMAX_DELAY);
+    Serial.printf("<== Consume: %d \n", i); fflush(stdout);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_RED, LOW);
+
+  xQueue = xQueueCreate(3, sizeof(int));
+  xTaskCreatePinnedToCore(taskProduce, "taskProduce", 4096, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(taskConsume, "taskConsume", 4096, NULL, 1, NULL, 1);
+  Serial.println("System ready");
+}
+
+void loop() {
+  vTaskDelay(0);
+}
